1b6b4100618b119a928359aab9a50aaafa2492da
nirmal jangid <jangidnirmal15@gmail.com>
2025-02-21 14:12:15 +0530

    sum of two number

---
+++ A2.java
@@ -0,0 +1,13 @@
+public class A2 {
+    int num1 = 4;
+    int num2 = 5;
+    int sum(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String args[]){
+        int num1 = 4;
+        int num2 = 5;
+        System.out.println(new A2().sum(num1,num2));
+    }
+}
